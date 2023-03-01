21d8d296ed728ca4539604f00903f5b611e36e0a
rrt2850 <79067594+rrt2850@users.noreply.github.com>
2023-03-01 18:10:39 -0500

    Optimized code and restructured some things

--- Client/src/Components/Inputs.js
+++ Client/src/Components/Inputs.js
@@ -1,55 +1,61 @@
-import React, { Component } from 'react'
+/**
+ * Inputs.js
+ * @author Robert Tetreault
+ * @summary Holds all the inputs for the website like the text area
+ *          and buttons. Also handles what happens when they're used
+ */
 
-export class Inputs extends Component {
+import React, { useState } from 'react';
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: ''
-        };
-        this.handleInput = this.handleInput.bind(this);
-        this.handleButtonClick = this.handleButtonClick.bind(this);
-        this.handleStartButtonClick = this.handleStartButtonClick.bind(this);
-    }
+const buttons = [
+    'Subway Surfers',
+    'Kinetic Sand',
+    'Soap Cutting',
+    'Hydraulic Press',
+    'Minecraft Parkour',
+    'Cake Decorating',
+    'Family Guy',
+    'Gameplay Footage',
+    'Random ASMR',
+    'Surprise Me'
+];
 
-    handleInput(event) {
-        this.setState({ value: event.target.value });
-    }
+const Inputs = ({ handleTyping, changePath, toRead }) => {
+    const [value, setValue] = useState('');
 
-    handleButtonClick(event) {
+    const handleInput = event => setValue(event.target.value);
+
+    const handleButtonClick = event => {
         const buttonIndex = parseInt(event.target.value, 10);
-        this.props.changePath(buttonIndex);
-    }
+        changePath(buttonIndex);
+    };
 
-    handleStartButtonClick() {
-        this.props.toRead(this.state.value);
-    }
+    const handleStartButtonClick = () => toRead(value);
 
-    render() {
-        return (
-            <div className='Inputs' onKeyDown={this.props.handleTyping}>
-                <textarea
-                    value={this.state.value}
-                    onChange={this.handleInput}
-                    placeholder="Type something here..."
-                ></textarea>
-                <div className='ButtonHolder'>
-                    <button type="button" value="0" onClick={this.handleButtonClick}>Subway Surfers</button>
-                    <button type="button" value="1" onClick={this.handleButtonClick}>Kinetic Sand</button>
-                    <button type="button" value="2" onClick={this.handleButtonClick}>Soap Cutting</button>
-                    <button type="button" value="3" onClick={this.handleButtonClick}>Hydraulic Press</button>
-                    <button type="button" value="4" onClick={this.handleButtonClick}>Minecraft Parkour</button>
-                    <button type="button" value="5" onClick={this.handleButtonClick}>Cake Decorating</button>
-                    <button type="button" value="6" onClick={this.handleButtonClick}>Family Guy</button>
-                    <button type="button" value="7" onClick={this.handleButtonClick}>Gameplay Footage</button>
-                    <button type="button" value="8" onClick={this.handleButtonClick}>Random ASMR</button>
-                    <button type="button" value="9" onClick={this.handleButtonClick}>Surprise Me</button>
-                    <button type="button" onClick={this.handleStartButtonClick}>Start</button>
-                </div>
-
+    return (
+        <div className='Inputs' onKeyDown={handleTyping}>
+            <textarea
+                value={value}
+                onChange={handleInput}
+                placeholder="Type something here..."
+            />
+            <div className='ButtonHolder'>
+                {buttons.map((buttonText, index) => (
+                    <button
+                        key={index}
+                        type="button"
+                        value={index}
+                        onClick={handleButtonClick}
+                    >
+                        {buttonText}
+                    </button>
+                ))}
+                <button type="button" onClick={handleStartButtonClick}>
+                    Start
+                </button>
             </div>
-        )
-    }
-}
+        </div>
+    );
+};
 
 export default Inputs;
--- Client/src/Components/Title.js
+++ Client/src/Components/Title.js
@@ -1,18 +1,25 @@
+/**
+ * Title.js
+ * @author Robert Tetreault
+ * @summary Displays the title and instructions for the website along with 
+ *          a message that only appears when the user isn't in fullscreen.
+ */
+
 import React, { useState, useEffect, useRef } from 'react';
 
 const Title = () => {
   const [isFullScreen, setIsFullScreen] = useState(false);
-  const header = useRef(null);
+  const headerRef = useRef(null);
 
   useEffect(() => {
-    const handleKeyDown = (event) => {
+    const handleKeyDown = event => {
       if (event.key === 'f' && event.target.tagName.toLowerCase() !== 'textarea') {
         toggleFullScreen();
       }
     };
 
     const handleFullScreenChange = () => {
-      setIsFullScreen(document.fullscreenElement !== null);
+      setIsFullScreen(Boolean(document.fullscreenElement));
     };
 
     document.addEventListener('keydown', handleKeyDown);
@@ -33,13 +40,10 @@ const Title = () => {
   };
 
   return (
-    <div ref={header} className="Title">
+    <div ref={headerRef} className="Title">
       <h1>Study Helper</h1>
       <h2>Enter text and select which videos you want playing to start</h2>
-      
-      {!isFullScreen && (
-        <div className='fullscreen-msg' id="fullscreen-msg">(Press F for the full experience)</div>
-      )}
+      {!isFullScreen && <div className="fullscreen-msg" id="fullscreen-msg">(Press F for the full experience)</div>}
     </div>
   );
 };
